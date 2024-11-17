f4e309e6e379910423d246caeefa97dc6f0aed24
EnnioFernandez <110144575+EnnioFernandez@users.noreply.github.com>
2024-11-17 02:03:20 -0300

    tareas

--- display.py
+++ display.py
@@ -1,9 +1,9 @@
-from machine import Pin, I2C, ADC
-from time import sleep_ms as delay
+from machine import Pin, ADC, I2C
+from time import sleep, time
 from lcd1602 import LCD
+import utime
 
-# Inicializa el bus I2C
+
+# Configuración de I2C para la pantalla LCD
 i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
-
-# Dirección del módulo LCD (cambiar si es distinta)
 lcd = LCD(i2c, addr=0x27)
