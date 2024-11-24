62dbbd755938c678aeb9672a53d6cb2ea5082bb9
EnnioFernandez <110144575+EnnioFernandez@users.noreply.github.com>
2024-11-24 20:42:55 -0300

    Terminado

--- Sensitiva.py
+++ Sensitiva.py
@@ -6,7 +6,7 @@ from i2c_lcd import I2cLcd
 
 # Configuración de pines
 Pines= [0, 2, 4, 15, 5, 17, 16, 18, 19, 
-        0, 23, 13, 12, 14, 27, 26, 25, 33, 32]
+        0, 13, 12, 14, 27, 26, 25, 33, 23, 32]
 
 # Pines de Salida digital DO
 DO = [Pin(pin, Pin.OUT) for pin in Pines[0:9]]  # Salidas digitales DO1 a DO10
@@ -37,11 +37,13 @@ Entradas = {
 
 # Pines de entrada analógica AI   
 AI1 = ADC(Pin(34))  # Potenciómetro P1 (regulación bajada EVB1)
-AI2 = ADC(Pin(36))  # Potenciómetro P2 (regulación división)
-AI3 = ADC(Pin(39))  # Potenciómetro P3 (regulación de la cantidad de cortes)
+AI2 = ADC(Pin(39))  # Potenciómetro P2 (regulación división)
+AI3 = ADC(Pin(36))  # Potenciómetro P3 (regulación de la cantidad de cortes)
+
 # Configuración de ADC para rango completo (0 a 3.3V)
 AI1.atten(ADC.ATTN_11DB)
 AI2.atten(ADC.ATTN_11DB)
+AI3.atten(ADC.ATTN_11DB)
 
 Potenciometro = {
     "P1"    :   AI1,    # Tiempo de bajada
@@ -53,13 +55,24 @@ Potenciometro = {
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)     #initializing the I2C method for ESP32
 lcd = I2cLcd(i2c, 0x27, 2, 16)
 
+# Interrupción para cambio de numero de cortes
+set_cortes= 0
+
+def Cortes():
+    global set_cortes
+    if leer("P3", 11)+1 != set_cortes:
+        print(int(leer("P3", 11)))
+        mostrar_mensaje("Nro de cortes:")
+        mostrar_mensaje("               ", 0, 1)
+        mostrar_mensaje(str(int(leer("P3",11))+1), 10, 1)
+        set_cortes = leer("P3", 11)+1
+
 #Función para determinar el tiempo transcurrido en segundos
 def time():
     return ticks_ms()/1000
 
 # Función para mostrar mensajes en pantalla
 def mostrar_mensaje(mensaje, fila=0, columna=0):
-    lcd.clear()
     lcd.move_to(fila, columna)
     lcd.putstr(mensaje)
     
@@ -102,22 +115,26 @@ def leer_potenciometro(ai, escala):
 def Secuencia_automatica():
     #Paso 1: Verificación de posición inicial
     if not verificar_posicion_inicial():
+        lcd.clear()
         mostrar_mensaje("Err1: falla posicion inicial")
         return
 
     # Paso 2: Acercamiento del carro rápidamente (R1, R3)
+    lcd.clear()
     mostrar_mensaje("Moviendo a K1")
     activar("Carro", "Vel rapida")    # Activar carro a velocidad rápida(R1 y R3)
     inicio_tiempo = time()  # Inicia tiempo de espera
-    #while not leer("K1").value():  # Espera por K1
-        #if time() - inicio_tiempo > 5:  # Timeout de 5 segundos
-            #mostrar_mensaje("Err2: Timeout K1")
-            #detener_todo()
-            #return
+    while not leer("K1"):  # Espera por K1
+        if time() - inicio_tiempo > 15:  # Timeout de 5 segundos
+            lcd.clear()
+            mostrar_mensaje("Err2: Timeout K1")
+            detener_todo()
+            return
 
     desactivar("Carro", "Vel rapida")  # Detener R1 y R3
 
     # Paso 3: Bajar herramienta (EVB2)
+    lcd.clear()
     mostrar_mensaje("Bajando herramienta")
     inicio_tiempo= time()
     activar("EV Down")  # Activar B2
@@ -128,33 +145,39 @@ def Secuencia_automatica():
     desactivar("EV Down")  # Detener B2
 
     # Paso 4: Encender husillo
+    lcd.clear()
     mostrar_mensaje("Encendiendo husillo")
     activar("Husillo")  # Activar husillo M1
 
     # Paso 5: Avanzar carro a velocidad controlada
+    lcd.clear()
     mostrar_mensaje("Avanzando a K2")
     activar("Carro")  # Activar avance (R1)
     inicio_tiempo = time()
     while not leer("K2"):  # Espera por K2
         if time() - inicio_tiempo > 25:  # Timeout de 25 segundos
+            lcd.clear()
             mostrar_mensaje("Error: Timeout K2")
             detener_todo()
             return
     desactivar("Carro")  # Detener avance
 
     # Paso 6: Apagar husillo
+    lcd.clear()
     mostrar_mensaje("Apagando husillo")
     desactivar("Husillo")
 
     # Paso 7: Subir herramienta (EVB2)
+    lcd.clear()
     mostrar_mensaje("Subiendo herramienta")
     activar("EV Up")  # Activar bobina B1
     while not leer("K4"):  # Espera por K4
-        delay(10)  # intervalo de espera
+        delay(1)  # intervalo de espera
     desactivar("EV Up")  # Detener B1
 
 
     # Paso 8: Activar división
+    lcd.clear()
     mostrar_mensaje("Inicio division")
     activar("Division")  # Activar división M4
     tiempo_division = leer("P2", 5)
@@ -164,24 +187,62 @@ def Secuencia_automatica():
     # Repetir desde el paso 1
     mostrar_mensaje("Corte completado")
     delay(1)
-    
-def monitoreo():
-    if leer("PT"):
-        pass
+ 
+
+
+def monitoreo(pin):
+    detener_todo()
+    while leer("PT") or leer("Stop") or leer("PS"):
+        if leer("PT"):
+            lcd.clear()
+            mostrar_mensaje("Error 1:")
+            mostrar_mensaje("Prot Termica", 0, 1)
+            delay(2)
+        
+        if leer("PS"):
+            lcd.clear()
+            mostrar_mensaje("Error 2:")
+            mostrar_mensaje("Presostato", 0, 1)
+            delay(2)
+
+        if leer("Stop"):
+            lcd.clear()
+            mostrar_mensaje("Error 3:")
+            mostrar_mensaje("PARADA", 0, 1)
+            delay(2)
+
+
+        lcd.clear()
+
+#   Interrupcion por PT:
+DI[5].irq(trigger=Pin.IRQ_RISING, handler=monitoreo)    
+
+#   Interrupción por PS:
+DI[6].irq(trigger=Pin.IRQ_RISING, handler=monitoreo)
+
+#   Interrupción por STOP:
+DI[9].irq(trigger=Pin.IRQ_RISING, handler=monitoreo)    
+
 
 
 def main():
+    mostrar_mensaje("Sensitiva ON")
     while True:
         if leer("Modo"):
+            print("Modo Automático OK")
             if leer("Start" ):
+                print("Start OK")
                 for i in range (leer("P1", 12)):
+                    print("Secuencia OK. Corte: ", i+1)
+                    Cortes()
                     Secuencia_automatica()
                 mostrar_mensaje("Ciclo terminado")
                 activar("Fin")
-                delay(3)
+                delay(1)
                 desactivar("Fin")
         
         else:
-            monitoreo()
+            print("Manual OK")
+            Cortes()
 
 main()
