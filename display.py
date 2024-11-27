4a2a07c36977038ccba683d58dc65fcac94975b7
EnnioFernandez <110144575+EnnioFernandez@users.noreply.github.com>
2024-11-27 00:02:55 -0300

    limpieza

--- display.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin, ADC, I2C
-from time import sleep, time
-from lcd1602 import LCD
-import utime
-
-
-# Configuración de I2C para la pantalla LCD
-i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
-lcd = LCD(i2c, addr=0x27)
